82d3dd3abcd959317898848053fbe0db01634a0f
yuichi1978 <rb19780519@gmail.com>
2022-05-10 09:51:20 +0900

    commit first

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,5 +1,12 @@
 import { memo, VFC } from "react";
+import { Wrap, WrapItem } from "@chakra-ui/react";
 
 export const UserManagement: VFC = memo(() => {
-  return <p>ユーザ管理ページです</p>
+  return (
+    <Wrap>
+      <WrapItem>
+        <div style={{ width: "100px", height: "100px", backgroundColor: "teal" }}></div>
+      </WrapItem>
+    </Wrap>
+  );
 });
